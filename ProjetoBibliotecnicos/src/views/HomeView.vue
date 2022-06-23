1931ddfcc1838f522ce5dd1e8a773ea1f317a20b
Gustavo P. Gorges <99231805+GustavodePaulaGorges@users.noreply.github.com>
2022-06-23 19:45:26 -0300

    Add files via upload

---
+++ ProjetoBibliotecnicos/src/views/HomeView.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="LivroWrapper">
+    <div class="BoxLivro">
+      Livro #1
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #2
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #3
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #4
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #5
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #6
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #7
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #8
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #9
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #10
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #11
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #12
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #13
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #14
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="BoxLivro">
+      Livro #15
+      <a class="pedir" href="https://chat.whatsapp.com/C7S38ddPKYLG97RStlAFGy" target="_blank"
+        >Pedir</a
+      >
+    </div>
+    <div class="Bottom"></div>
+  </div>
+</template>
+<style>
+.pedir{
+  margin-left: 1vh;
+}
+.Bottom {
+  height: 10vh;
+}
+.BoxLivro {
+  width: 100%;
+  box-shadow: 0.5px 0.5px 2.5px black;
+  padding: 1rem;
+  margin-bottom: 1vh;
+}
+
+</style>
